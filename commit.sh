445a51837c6800a3510e7a8c5f1f7f50031f798c
salah <@salah>
2026-02-09 15:32:13 +0330

    test

--- commit.sh
+++ commit.sh
@@ -15,7 +15,7 @@ git stash
 # git config pull.rebase false
 
 # 4. Pull the latest changes from GitHub
-git pull origin v4
+git pull origin develop
 
 # 5. Bring your changes back from the stash
 git stash pop
@@ -25,4 +25,4 @@ git commit -m "test" -a
 
 
 # 7. Push to GitHub
-git push origin v4
+git push origin develop
